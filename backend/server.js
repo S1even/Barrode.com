566f7169ac03bd7a71b651716ac9f3dfd07e4094
S1even <Stevendevin95520@hotmail.com>
2025-04-29 19:29:57 +0200

    Update server.js for deploy

--- backend/server.js
+++ backend/server.js
@@ -40,8 +40,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
+    secure: true,
+    ssameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000, // 1 jour
   }
 }));

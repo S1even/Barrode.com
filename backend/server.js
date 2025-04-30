27872398151f17dd9c5ddec1e5940d4f951ef561
S1even <Stevendevin95520@hotmail.com>
2025-04-30 11:16:03 +0200

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,75 +1,70 @@
 const express = require("express");
-require("dotenv").config();
-const session = require("express-session");
-const passport = require("passport");
-require("./config/passport");
-
-const connectDB = require("./config/db");
-const cookieParser = require("cookie-parser");
-const { checkUser, requireAuth } = require("./middleware/auth.middleware");
-const cors = require("cors");
-
-const port = process.env.PORT || 5001;
-const userRoutes = require("./routes/user.routes");
-const postRoutes = require("./routes/post.routes");
-
-connectDB();
-
-const app = express();
-
-
-const corsOptions = {
-  origin: process.env.CLIENT_URL || "https://barrode-com.vercel.app",
-  credentials: true,
-  allowedHeaders: ['sessionId', 'Content-Type', 'Authorization'],
-  exposedHeaders: ['sessionId'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  preflightContinue: false
-};
-app.use(cors(corsOptions));
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-
-app.use(cookieParser());
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // true en production
-    sameSite: 'none', // Important pour cross-domain cookies
-    maxAge: 3 * 24 * 60 * 60 * 1000 // 3 jours
-  }
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes d'authentification Google
-app.use("/auth", require("./routes/auth.routes"));
-
-
-// Route pour vérifier le JWT
-app.get("/jwtid", requireAuth, (req, res) => {
-  res.status(200).send(res.locals.user._id);
-});
-
-
-app.use("/api/post", postRoutes);
-app.use("/api/user", userRoutes);
-
-app.use("/uploads", express.static("uploads"));
-
-
-
-app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
-});
-
-
-app.listen(port, () => console.log(`Serveur démarré sur le port ${port}`));
+ require("dotenv").config();
+ const session = require("express-session");
+ const passport = require("passport");
+ require("./config/passport");
+ 
+ const connectDB = require("./config/db");
+ const cookieParser = require("cookie-parser");
+ const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+ const cors = require("cors");
+ 
+ const port = process.env.PORT || 5001;
+ const userRoutes = require("./routes/user.routes");
+ const postRoutes = require("./routes/post.routes");
+ 
+ connectDB();
+ 
+ const app = express();
+ 
+ 
+ const corsOptions = {
+   origin: process.env.CLIENT_URL || "https://barrode-com.vercel.app",
+   credentials: true,
+   allowedHeaders: ['sessionId', 'Content-Type', 'Authorization'],
+   exposedHeaders: ['sessionId'],
+   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+   preflightContinue: false
+ };
+ app.use(cors(corsOptions));
+ 
+ 
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: true }));
+ app.use(cookieParser());
+ 
+ 
+ app.use(session({
+   secret: process.env.SESSION_SECRET || "monSecret",
+   resave: false,
+   saveUninitialized: false,
+   cookie: {
+     httpOnly: true,
+     secure: true,
+     ssameSite: 'none',
+     maxAge: 24 * 60 * 60 * 1000, // 1 jour
+   }
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ 
+ // Routes d'authentification Google
+ app.use("/auth", require("./routes/auth.routes"));
+ 
+ 
+ // Route pour vérifier le JWT
+ app.get("/jwtid", requireAuth, (req, res) => {
+   res.status(200).send(res.locals.user._id);
+ });
+ 
+ 
+ app.use("/api/post", postRoutes);
+ app.use("/api/user", userRoutes);
+ 
+ app.use("/uploads", express.static("uploads"));
+ 
+ 
+ 
+ app.use((req, res) => {
+   res.status(404).json({ error: "Route not found" });
+ });
